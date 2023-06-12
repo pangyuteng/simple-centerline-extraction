90941b4b0ae67565d59c9f8a7d96e5d44d416b7d
pangyuteng <pangyuteng@gmail.com>
2023-06-12 13:11:17 +0000

    added extract_slice

--- test.py
+++ test.py
@@ -36,52 +36,74 @@ class MyTest(unittest.TestCase):
         self.assertTrue(all(np.logical_or(np.gradient(actual_z)==0,np.gradient(actual_z)<0)))
 
     def test_extract_slice(self):
+
+
         from extract_centerline import extract_centerline, smooth_3d_array
         from extract_slice import extract_slice
 
         img,x,y,z = mock_image()
-        img=binary_dilation(img,iterations=5)
 
         start_point = np.array([x[0],y[0],z[0]]).astype(int)
-        end_point = np.array([x[-1],y[-1],z[-1]]).astype(int)        
-        centerline_list = extract_centerline(img,start_point,end_point)
-        actual_x, actual_y, actual_z = centerline_list
-
-
-        bsfield = distance_transform_edt(img)
-        gaussian = sitk.SmoothingRecursiveGaussianImageFilter()
-        gaussian.SetSigma(2)
+        end_point = np.array([x[-1],y[-1],z[-1]]).astype(int)
+        search_radius=(1,1,1)
+        centerline_list = extract_centerline(img,start_point,end_point,search_radius=search_radius)
+        c_x,c_y,c_z = centerline_list
+        c_x,c_y,c_z = c_x[::20],c_y[::20],c_z[::20]
+        smothing_factor = 100
+        s_x,s_y,s_z = smooth_3d_array(c_x,c_y,c_z,s=smothing_factor)
+        offset = 1
         
+        bsfield = distance_transform_edt(img)
         img_obj = sitk.GetImageFromArray(bsfield)
         img_obj.SetOrigin((0,0,0))
         img_obj.SetDirection((1,0,0,0,1,0,0,0,1))
         img_obj.SetSpacing((1,1,1))
-        img_obj = gaussian.Execute(img_obj)
         tmp = sitk.GetArrayFromImage(img_obj)
 
-        for idx in range(len(actual_x)):
-            if idx == 0 or idx == len(actual_x)-1:
-                continue
-            coord_pre = (int(actual_z[idx-1]), int(actual_y[idx-1]), int(actual_x[idx-1]))
-            coord_mid = (int(actual_z[idx]), int(actual_y[idx]), int(actual_x[idx])) # numpy to itk
-            coord_post = (int(actual_z[idx+1]), int(actual_y[idx+1]), int(actual_x[idx+1]))
+        idx = 4
+        slice_center = int(c_z[idx  ]),int(c_y[idx  ]),int(c_x[idx  ])
+        before_point = int(s_z[idx-offset]),int(s_y[idx-offset]),int(s_x[idx-offset])
+        after_point  = int(s_z[idx+offset]),int(s_y[idx+offset]),int(s_x[idx+offset])
 
-            point_pre = img_obj.TransformContinuousIndexToPhysicalPoint(coord_pre)
-            point_post = img_obj.TransformContinuousIndexToPhysicalPoint(coord_post)
-            slice_normal = np.array(point_post)-np.array(point_pre)
-            slice_center = img_obj.TransformContinuousIndexToPhysicalPoint(coord_mid)
-            slice_spacing = (0.5,0.5,0.5)
-            slice_radius = 20
-            is_label = False
-            slice_obj = extract_slice(img_obj,slice_center,slice_normal,slice_spacing,slice_radius,is_label)
-            arr = sitk.GetArrayFromImage(slice_obj)
-            print(bsfield[actual_x[idx-1],actual_y[idx-1],actual_z[idx-1]])
-            print(slice_center,slice_normal)
-            print(arr.shape,arr.dtype,np.min(arr),np.max(arr))
-            arr = arr.squeeze()
-            arr = 255*((arr-np.min(arr))/(np.max(arr)-np.min(arr))).clip(0,1)
-            arr = arr.astype(np.uint8)
-            imageio.imwrite(f'slice-{idx}.png',arr)
+        appx_radius = int(bsfield[c_x[idx],c_y[idx],c_z[idx]])
+
+
+        tmp = sitk.GetArrayFromImage(img_obj)
+
+        expected_value = 1000
+        tmp[c_x[idx],c_y[idx],c_z[idx]]=expected_value
+        tmp_obj = sitk.GetImageFromArray(tmp)
+        tmp_obj.CopyInformation(img_obj)
+
+        slice_center = tmp_obj.TransformContinuousIndexToPhysicalPoint(slice_center)
+        before_point = tmp_obj.TransformContinuousIndexToPhysicalPoint(before_point)
+        after_point = tmp_obj.TransformContinuousIndexToPhysicalPoint(after_point)
+
+        slice_normal = np.array(after_point)-np.array(before_point) # not normalized
+        slice_spacing = (1,1,1) #mm
+        slice_radius = appx_radius
+        is_label = False
         
+        slice_obj = extract_slice(tmp_obj,slice_center,slice_normal,slice_spacing,slice_radius,is_label)    
+        slice_arr = sitk.GetArrayFromImage(slice_obj)
+        assert(slice_arr[0,appx_radius,appx_radius]-expected_value < 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
